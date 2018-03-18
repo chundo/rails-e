dd4814ef3e236d7126e24738b780e2af4503377a
chundo <segundo.espana1@gmail.com>
2018-03-18 14:29:52 -0500

    Configuration root and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
+  authenticated :user do
+    root 'home#index'
+  end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  unauthenticated :user do
+    devise_scope :user do
+      root 'home#unregistered', as: :unregistered_root
+    end
+  end
+
 end

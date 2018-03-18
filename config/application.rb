e684f93193d395b12ee06cff4dd5844afc240926
chundo <segundo.espana1@gmail.com>
2018-03-18 18:33:25 -0500

    Assets, style and render for layouts

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module RailsE
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << Rails.root.join("public", "public/*", "*/public", "*/public/*")
   end
 end
